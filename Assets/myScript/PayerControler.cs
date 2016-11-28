2c74a46e13f49ccb7942b83a2cb3ce50d9318a43
Esame <evan@clanmclean.net>
2016-11-28 07:40:36 -0800

    wall jumping

--- Assets/myScript/PayerControler.cs
+++ Assets/myScript/PayerControler.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PayerControler : MonoBehaviour {
 
 	public bool facingRight = true;	
+	public int facing = 1;
 	public bool jump = false;	
 
 	Animator animator;
@@ -11,25 +12,31 @@ public class PayerControler : MonoBehaviour {
 	public float moveForce = 365f;
 	public float maxSpeed = 5f;
 	public float jumpForce = 1000f;
+	public float wallForce = 50000f;
 	const int STATE_IDLE = 0;
 	const int STATE_WALK = 1;
 	const int STATE_JUMP = 2;
+	const int STATE_ATTACK = 3;
 	int _currentAnimationState = STATE_IDLE;
 	private Transform groundCheck;
+	private Transform wallCheck;
 	private bool grounded = false;
-
+	private bool wallTrump = false;
+	int shotCount = 250;
 	void Awake()
 	{
 		groundCheck = transform.Find("groundCheck");
 		animator = this.GetComponent<Animator>();
+		wallCheck = transform.Find("wallCheck");
 	}
 	
 	void Update()
 	{
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
-
-		if(Input.GetButtonDown("Jump") && grounded)
-			jump = true;
+		wallTrump = Physics2D.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+			
+		if (Input.GetButtonDown ("Jump") && (wallTrump || grounded)) 
+				jump = true; 
 	}
 
 	void FixedUpdate ()
@@ -37,7 +44,8 @@ public class PayerControler : MonoBehaviour {
 		float h = Input.GetAxis("Horizontal");
 
 		if (h * GetComponent<Rigidbody2D> ().velocity.x < maxSpeed) {
-			GetComponent<Rigidbody2D> ().AddForce (Vector2.right * h * moveForce);
+			if (grounded) GetComponent<Rigidbody2D> ().AddForce (Vector2.right * h * moveForce);
+			else GetComponent<Rigidbody2D> ().AddForce (Vector2.right * h * moveForce/10);
 		}
 
 		if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > 0 && grounded)
@@ -53,15 +61,31 @@ public class PayerControler : MonoBehaviour {
 			Flip();
 		else if(h < 0 && facingRight)
 			Flip();
-
+		
 		if(jump)
 		{
 			changeState(STATE_JUMP);
 
+			if (wallTrump) { //if you're next to a wall 
+				Flip ();
+				GetComponent<Rigidbody2D> ().AddForce (new Vector2 ((wallForce * facing), 0f));
+
+			}
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
 
 			jump = false;
 		}
+		if (GetComponent<Rigidbody2D>().position.y < -10) {
+			transform.position = new Vector3 (0, 1, 0);
+		}
+		/*
+		if (jump && shotCount == 0) {
+			changeState (STATE_ATTACK);
+			shotCount += 250;
+		} else if (shotCount > 0) {
+			shotCount -= 1;
+		}
+*/ //duncan's test code
 	}
 
 	void Flip ()
@@ -71,6 +95,7 @@ public class PayerControler : MonoBehaviour {
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+		facing *= -1;
 	}
 
 	void changeState(int state){
@@ -90,6 +115,10 @@ public class PayerControler : MonoBehaviour {
 
 		case STATE_IDLE:
 			animator.SetInteger ("state", STATE_IDLE);
+			break;
+
+		case STATE_ATTACK:
+			animator.SetInteger ("state", STATE_ATTACK);
 			break;
 		}
 
